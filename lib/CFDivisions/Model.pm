f90f3b6b800add94c0aa307619db44fa077e053f
Sascha Dibbern <sascha@dibbern.info>
2016-05-15 14:01:26 +0200

    add Model

---
+++ lib/CFDivisions/Model.pm
@@ -0,0 +1,154 @@
+package CFDivisions::Model;
+
+use strict;
+use warnings;
+use v5.14;
+
+use Data::Dumper;
+use CFDivisions::Utils;
+use Carp;
+
+sub new {
+    my $class = shift;
+    my %args  = @_;
+
+    my $divs  = $args{divisions} // croak "No 'divisions' argument";
+    croak "'divisions' argument must be hashref" 
+	unless ref($divs) eq 'HASH';
+
+    my $deps  = $args{dependencies} // croak "No 'dependencies' argument";
+    croak "'dependencies' argument must be hashref" 
+	unless ref($deps) eq 'HASH';
+
+    my $self  = {
+	divisions                => $divs,
+	dependencies             => $deps,
+
+	divisionorder            => undef,
+	divisionordered          => {},
+
+	# checklist for preventing circular division dependencies
+	circular_div_ref_stack   => [],
+
+	verbose                  => $args{verbose},
+    };
+
+    bless $self, $class;
+}
+
+sub assert_no_circular_division_reference {
+    my $self  = shift;
+
+    my $stack = $self->{circular_div_ref_stack};
+    my %count;
+    for my $stacked_division (@$stack) {
+	$count{$stacked_division}++;
+	croak "Circular division dependencies: ".join(",",@$stack)
+	    if $count{$stacked_division}>1;	
+    }
+}
+
+sub add_to_circular_division_ref_investigation {
+    my $self     = shift;
+    my $division = shift;
+    my $stack    = $self->{circular_div_ref_stack};
+    push @$stack,$division;
+}
+
+sub remove_from_circular_division_ref_investigation {
+    my $self     = shift;
+    my $division = pop @{$self->{circular_div_ref_stack}};
+}
+
+sub assert_existing_division_dependencies {
+    my $self     = shift;
+    my $division = shift;
+    my $deps     = shift;
+
+    # not necessary due to circular ref check
+    # delete $self->{dependencies}->{$division};
+
+    foreach my $dep (@$deps) {
+	croak "Illegal division dependency '$dep' in division '$division'"
+	    unless exists $self->{divisions}->{$dep};
+    }
+}
+
+sub assert_existing_division {
+    my $self     = shift;
+    my $division = shift // croak "No division";
+
+    croak "Nonexisting division" 
+	unless defined $self->{divisions}->{$division};
+}
+
+sub add_division_to_divisionorder {
+    my $self     = shift;
+    my $division = shift // croak "No division";
+
+    $self->assert_existing_division($division);
+    push @{$self->{divisionorder}},$division;
+    $self->{divisionordered}->{$division} = 1;
+}
+
+sub is_division_in_divisionorder {
+    my $self     = shift;
+    my $division = shift // croak "No division";
+
+    return $self->{divisionordered}->{$division};
+}
+
+#
+# Investigate the order of divisions for later handling of the total bundlesequence
+#
+sub add_divisiontree_to_divisionorder {
+    my $self     = shift;
+    my $division = shift // croak "No division";
+
+    # Case 1: division is already in divisionorder
+    return 0 if $self->is_division_in_divisionorder($division);
+    
+    $self->add_to_circular_division_ref_investigation($division);
+    $self->assert_no_circular_division_reference();
+
+    my $dependencies = $self->{dependencies}->{$division};
+
+    # Case 2: Division has no dependencies
+    unless (defined $dependencies) {
+	$self->add_division_to_divisionorder($division);
+
+	$self->remove_from_circular_division_ref_investigation;
+	return 1;
+    }
+
+    $self->assert_existing_division_dependencies($division,$dependencies);
+  
+    # Recurse into dependencies 
+    map { 
+	$self->add_divisiontree_to_divisionorder($_);
+    } @$dependencies;
+    
+    $self->add_division_to_divisionorder($division);
+
+    # remove division from checklist
+    $self->remove_from_circular_division_ref_investigation;
+}
+
+sub divisionorder {
+    my $self = shift;
+
+    unless (defined $self->{divisionorder}) {
+	$self->{divisionorder} = [];
+
+	my @divisions=keys %{$self->{divisions}};
+	for my $division (@divisions) {
+	    $self->add_divisiontree_to_divisionorder($division);
+	}
+    }
+
+    return wantarray ?
+	@{$self->{divisionorder}} :
+	$self->{divisionorder};
+}
+
+1;
---
+++ t/Model.t
@@ -0,0 +1,114 @@
+#!/bin/env perl
+
+use strict;
+use warnings;
+use Test::More;
+use Test::Exception;
+use Data::Dumper;
+use v5.14;
+
+use CFDivisions::Model;
+BEGIN { use_ok( 'CFDivisions::Model' ); }
+
+subtest "Constructor" => sub {
+    dies_ok {
+	my $model = CFDivisions::Model->new(
+	    divisions     => undef,
+	    );
+    } "Fail when no divisions";
+    
+    dies_ok {
+	my $model = CFDivisions::Model->new(
+	    divisions     => {},
+	    dependencies  => undef,
+	    );
+    } "Fail when no dependencies";
+    
+
+    my $model = CFDivisions::Model->new(
+	divisions     => {a=>'a',b=>'b'},
+	dependencies  => {b=>['a']},
+	);
+    ok(defined $model,"Created model");
+};
+
+subtest "Circular division reference investigation" => sub {
+    my $model = CFDivisions::Model->new(
+	divisions     => {},
+	dependencies  => {},
+	);
+    $model->add_to_circular_division_ref_investigation('1');
+    lives_ok { $model->assert_no_circular_division_reference() }"Assert no circular reference 1";
+
+    $model->add_to_circular_division_ref_investigation('2');
+    lives_ok { $model->assert_no_circular_division_reference() } "Assert no circular reference 2";
+
+    $model->add_to_circular_division_ref_investigation('1');
+    dies_ok { $model->assert_no_circular_division_reference() } "Failed assert no circular reference 1";
+
+    $model->remove_from_circular_division_ref_investigation('1');
+    lives_ok { $model->assert_no_circular_division_reference() } "Assert no circular reference 1";
+
+};
+
+subtest "assert_existing_division_dependencies" => sub {
+    my $model = CFDivisions::Model->new(
+	divisions     => {a=>'1',b=>'2'},
+	dependencies  => {b=>['a']},
+	);
+
+    lives_ok { $model->assert_existing_division_dependencies('b',['a']); } "Valid input data";
+
+    dies_ok { $model->assert_existing_division_dependencies('b',['c']); } "Invalid input data: nonexisting division";
+};
+
+subtest "Divisionorder primitives" => sub {
+    my $model = CFDivisions::Model->new(
+	divisions     => {a=>'1',b=>'2'},
+	dependencies  => {},
+	);
+
+    lives_ok { $model->assert_existing_division('a'); } "Success asserting existing division";
+    dies_ok { $model->assert_existing_division('c'); } "Failed asserting existing division";
+
+    $model->add_division_to_divisionorder('a');
+    ok($model->is_division_in_divisionorder('a'),"Check one division in order");
+
+    $model->add_division_to_divisionorder('b');
+    ok($model->is_division_in_divisionorder('b'),"Check second division in order");
+
+    my $d_order = $model->divisionorder();
+    is_deeply(
+	$d_order,
+	['a','b'],
+	"Right division order registration"
+	);
+};
+
+subtest "Divisionorder building" => sub {
+    my $model = CFDivisions::Model->new(
+	divisions     => {a=>'1',b=>'2',c=>'3',d=>'4'},
+	dependencies  => {
+	    b=>['a'],
+	    c=>['b'],
+	    d=>['b','c'],
+	}, # expected order -> a,b,c,d
+	);
+    
+    my @order = $model->divisionorder();
+    is_deeply(
+	\@order,
+	[
+	 'a',
+	 'b',
+	 'c',
+	 'd'
+	],
+	"Right division order"
+	);
+    #say Dumper($model);
+
+};
+
+done_testing;
+
